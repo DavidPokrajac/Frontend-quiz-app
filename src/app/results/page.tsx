7769e82fd223e924298355f87f9b759106a55aa2
David <d0pokrajac@gmail.com>
2025-04-23 10:41:28 +0200

    Added Suspense Boundary

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -4,6 +4,7 @@ import Header from "../components/Header"
 import { useSearchParams } from "next/navigation"
 import ResultSummary from "../components/ResultSummary"
 import Outro from "../components/Outro"
+import { Suspense } from "react"
 
 export default function Results() {
   const searchParams = useSearchParams()
@@ -11,12 +12,12 @@ export default function Results() {
   const rightAnswers = searchParams.get("rightAnswers") as string
 
   return (
-    <>
+    <Suspense>
       <Header subject={search} />
       <main className="lg:grid-rows-[1fr row-start-2 grid grid-rows-[min-content_310px] gap-[40px] px-[1.5em] pt-[2em] lg:grid-cols-[repeat(2,_1fr)] lg:grid-rows-[1fr]">
         <Outro />
         <ResultSummary search={search} rightAnswers={rightAnswers} />
       </main>
-    </>
+    </Suspense>
   )
 }
