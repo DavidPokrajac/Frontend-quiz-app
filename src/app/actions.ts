f2ec398cf2570ca91604c83b2ff0fd9be33493f2
David <d0pokrajac@gmail.com>
2025-04-25 16:35:50 +0200

    Fixed type

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation"
 export async function create() {
   try {
     const file = await fs.readFile(process.cwd() + "/data.json", "utf8")
-    if (file !== "undefined" && file !== null) {
+    if (file !== undefined && file !== null) {
       const data = JSON.parse(file)
       return data
     }
