a4bc577fef8f2c52a2d7e3fdae41aa6b6f6714d7
David <d0pokrajac@gmail.com>
2025-04-27 14:30:34 +0200

    Removed try catch

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,15 +4,10 @@ import { promises as fs } from "fs"
 import { redirect } from "next/navigation"
 
 export async function create() {
-  try {
-    const file = await fs.readFile(process.cwd() + "/data.json", "utf8")
-    if (file !== undefined && file !== null) {
-      const data = JSON.parse(file)
-      return data
-    }
-  } catch (error) {
-    console.error(error)
-  }
+  const file = await fs.readFile(process.cwd() + "/data.json", "utf8")
+
+  const data = JSON.parse(file)
+  return data
 }
 
 export async function navigate(data: string, rightAnswers: number) {
